5067dc55a8d3e264527ad279f69e5a1935ae2861
Shuyang <l.shuyang98@gmail.com>
2021-10-11 13:09:14 +0800

    Collect static files

--- collectify/settings.py
+++ collectify/settings.py
@@ -18,6 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
