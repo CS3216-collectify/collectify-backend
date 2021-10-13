fb575f28ca9d1070b548bf4040db0bf11e5397a1
Shuyang <l.shuyang98@gmail.com>
2021-10-14 06:41:01 +0800

    Implement deleting/adding images for "update item" endpoint

--- collects/views.py
+++ collects/views.py
@@ -68,6 +68,7 @@ class ItemViewSet(viewsets.ModelViewSet):
 
         return queryset
 
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -85,10 +86,30 @@ class ItemViewSet(viewsets.ModelViewSet):
         collection = self.kwargs.get('collections_pk')
         serializer.save(collection=Collect.objects.get(id=collection))
 
-    def update(self, request, *args, **kwargs):
-        super(ItemViewSet, self).update(request, *args, **kwargs)
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        
+        if request.data.get('deleted_image_ids'):
+            for image_id in request.data.get('deleted_image_ids'):
+                Image.objects.get(id=int(image_id)).delete()
+        
+        files = request.FILES.getlist('new_images')
+        for file in files:
+            image = Image(image_file=file, item=serializer.instance)
+            image.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
