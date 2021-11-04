4e0271a065fcb7c0bc1d81c211130396bdd4a9c7
Shuyang <l.shuyang98@gmail.com>
2021-11-05 06:20:43 +0800

    Resolve eb healthcheck status

--- collectify/settings.py
+++ collectify/settings.py
@@ -27,7 +27,16 @@ SECRET_KEY = collectifysecrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1', '172.31.0.75', 'collectify.link', '46.137.226.252', '54.254.69.227']
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1', 'collectify.link', '46.137.226.252', '54.254.69.227']
+
+import requests
+try:
+    internal_ip = requests.get('http://instance-data/latest/meta-data/local-ipv4').text
+except requests.exceptions.ConnectionError:
+    pass
+else:
+    ALLOWED_HOSTS.append(internal_ip)
+del requests
 
 # AWS S3
 AWS_ACCESS_KEY_ID = collectifysecrets.AWS_ACCESS_KEY_ID
